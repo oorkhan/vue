17671be8cc469c0edeee876966ed7167714a3af4
Orkhan Orujaliyev <o.orkhan@gmail.com>
2019-01-07 23:05:02 +0400

    2304 07012019

--- vuemastery/lib/main.js
+++ vuemastery/lib/main.js
@@ -7,24 +7,37 @@ document.addEventListener("DOMContentLoaded", function() {
 let app01 = new Vue({
   el: "#root01",
   data: {
-    product: "Red Socks",
+    product: "Socks",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, non!",
     url: "https://excisionmerch.com/products/bass-canyon-midnight-socks",
     image: "images/socks.jpg",
     stock: 10,
+    inStock: true,
     onSale: true,
-    details:['80 % cotton', '20 % polyester', 'for men'],
-    variants:[
+    details: ["80 % cotton", "20 % polyester", "for men"],
+    variants: [
       {
-        variantId:1,
-        variantColor: 'blue'
+        variantId: 1,
+        variantColor: "blue",
+        variantImage: "images/socks.jpg"
       },
       {
-        variantId:2,
-        variantColor: 'green'
+        variantId: 2,
+        variantColor: "green",
+        variantImage: "images/socks_blue.jpg"
       }
     ],
-    sizes:['small', 'medium', 'large', 'XL']
+    sizes: ["small", "medium", "large", "XL"],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    changeImage(variantImage){
+      this.image = variantImage;
+      console.log(this.variantImage);
+    }
   }
 });
